c8c517a66d36c871648cc2e7e28a4693f5acc5b1
Mike Young <mjyoungen@gmail.com>
2024-05-21 14:36:10 -0400

    Add react-router-dom. Change artifact path to 'public'

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+
 import App from './App';
 import theme from './theme';
 
@@ -12,6 +14,8 @@ root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ThemeProvider>,
 );
