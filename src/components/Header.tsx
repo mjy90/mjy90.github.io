3e4ccbe214183604b75fd60f3a304ab9e696512c
Mike Young <35304678+mjy90@users.noreply.github.com>
2024-05-23 01:35:13 -0400

    Add Resume page (#2)
    * Add resume and react-pdf. Create Resume component.
    
    * Format PDF
    
    * Fix header somewhat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,6 @@ import {
   Typography,
   Menu,
   Container,
-  Avatar,
   Tooltip,
   MenuItem,
 } from '@mui/material';
@@ -22,11 +21,9 @@ const pages: { title: string, path: string }[] = [
   { title: 'Projects', path: 'projects' },
   { title: 'Blog', path: 'blog' },
 ];
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -35,55 +32,37 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
-  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
-
   return (
     <AppBar position="static">
       <Container>
         <Toolbar disableGutters>
-          <Box mr={2}>
-            <img
-              src={Monogram}
-              alt="Mike's monogram"
-              style={{
-                height: '50px',
-              }}
-            />
-          </Box>
-          <LinkButton to='/'>
+          <LinkButton to='/' color='secondary'>
             <Typography
               variant="h6"
               noWrap
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
                 fontFamily: 'Montserrat',
                 fontWeight: 400,
                 letterSpacing: '.3rem',
-                color: 'inherit',
                 textDecoration: 'none',
               }}
             >
-              Mike
+              mikeyoung.tech
             </Typography>
           </LinkButton>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-          {/* <IconButton
+            <IconButton
               size="large"
-              aria-label="account of current user"
+              aria-label="navigation menu"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
               color="inherit"
             >
               <MenuIcon />
-            </IconButton> */}
+            </IconButton>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -111,11 +90,11 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <Typography
+          {/* <Typography
             variant="h5"
           >
-            Mike
-          </Typography>
+            mikeyoung.tech
+          </Typography> */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page, index) => (
               <LinkButton
@@ -128,36 +107,6 @@ function ResponsiveAppBar() {
               </LinkButton>
             ))}
           </Box>
-
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
